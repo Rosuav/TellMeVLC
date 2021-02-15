d32f18fd3fd1bcee61a639b33af9100bddbc099c
Chris Angelico <rosuav@gmail.com>
2021-02-16 03:54:44 +1100

    Flesh out some stubs, stub out some more

--- tellmevlc.c
+++ tellmevlc.c
@@ -2,28 +2,60 @@
 #include <vlc_common.h>
 #include <vlc_plugin.h>
 #include <vlc_interface.h>
-#include <stdio.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
 
 #define MAX_SOCKETS 256
 struct intf_sys_t {
+	vlc_thread_t thread;
 	int nsock; //Will generally be at least 1 (the main socket)
-	int sockets[MAX_SOCKETS];
+	struct pollfd sockets[MAX_SOCKETS];
 };
 
-static int Open(vlc_object_t *this) {
-	printf("Hello, world!\n");
+static void *Run(void *this) {
 	intf_thread_t *intf = (intf_thread_t *)this;
-	intf_sys_t *sys = malloc(sizeof(intf_sys_t));
-	if (!sys) return VLC_ENOMEM;
-	//if (thing failed) {free(sys); return VLC_EGENERIC;}
-	intf->p_sys = sys;
-	return VLC_SUCCESS;
+	intf_sys_t *sys = intf->p_sys;
+	while (1) {
+		msg_Info(intf, "Hello, world!");
+		poll(sys->sockets, sys->nsock, -1);
+	}
+	return 0;
 }
 
 static void Close(vlc_object_t *this) {
 	intf_thread_t *intf = (intf_thread_t *)this;
 	intf_sys_t *sys = intf->p_sys;
+	struct pollfd *sock = sys->sockets; int n = sys->nsock;
+	while (n--) {close(sock->fd); ++sock;}
 	free(sys);
+}
+
+static int Open(vlc_object_t *this) {
+	intf_thread_t *intf = (intf_thread_t *)this;
+	intf_sys_t *sys = malloc(sizeof(intf_sys_t));
+	if (!sys) return VLC_ENOMEM;
+	int mainsock = socket(AF_INET6, SOCK_STREAM, 0);
+	int port = 4221; //TODO: Allow this to be configured
+	struct sockaddr_in6 bindto = {AF_INET6, htons(port), 0, in6addr_any, 0};
+	if (bind(mainsock, (struct sockaddr *)&bindto, sizeof(bindto))) {
+		msg_Err(this, "%s on port %d", vlc_strerror(errno), port);
+		close(mainsock);
+		free(sys);
+		return VLC_EGENERIC;
+	}
+	listen(mainsock, 5);
+	sys->sockets[0].fd = mainsock;
+	sys->sockets[0].events = POLLIN;
+	sys->nsock = 1;
+	msg_Info(this, "Listening on %d.", port);
+	intf->p_sys = sys;
+	if (vlc_clone(&sys->thread, Run, intf, VLC_THREAD_PRIORITY_LOW)) {
+		Close(this);
+		return VLC_EGENERIC;
+	}
+	return VLC_SUCCESS;
 }
 
 vlc_module_begin()
